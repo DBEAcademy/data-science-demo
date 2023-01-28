1fd27cf91775e70cec2b1ba3e20126d118a056c9
DBE Academy <95634648+DBEAcademy@users.noreply.github.com>
2023-01-28 13:24:15 +0100

    Create write.csv.py
    How to Create CSV File in Python?

---
+++ code/write.csv.py
@@ -0,0 +1,6 @@
+import csv
+rows = [ ["x","y"],["z","v"]]
+with open('myfile.csv', 'w', newline='') as file:
+  mywriter = csv.writer(file, delimiter=',')
+  mywriter.writerows(rows)
+  mywriter.writerow(["ZZ", "FF"])
